88021ef3d480121f9c0bbecffb4b3c5faf1b29f4
AngelsGV <angels.not99@gmail.com>
2023-11-19 21:18:00 +0100

    Initial commit

--- src/Actividad17.java
+++ src/Actividad17.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 public class Actividad17 {
     public static void main(String[] args){
+        // Principal. Primero pido números y esclarezco que hacer con la función.
         Scanner sc = new Scanner(System.in);
-        int num1, num2;
+        int num1, num2; // Vamos a trabajar con números enteros para sacar los divisores.
         do {
             System.out.println("Escribe un número: ");
             num1 = sc.nextInt();
-            if (num1 <= 0 ){
+            if (num1 <= 0 ){ //Si el num1 es menor o igual a 0 da mensaje de error y volvera a pedirlo.
                 System.out.println("Error. Los números tienen que ser positivos.");
             }
         }while(num1 <= 0);
@@ -14,11 +15,13 @@ public class Actividad17 {
         do{
             System.out.println("Escribe otro número: ");
             num2 = sc.nextInt();
-            if (num2 <= 0){
+            if (num2 <= 0){//Si el num2 es menor o igual a 0 da mensaje de error y volvera a pedirlo.
                 System.out.println("Error. Los números tienen que ser positivos.");
             }
         }while(  num2 <= 0);
         //Bucle 2 para pedir número y que sea positivo. Para no tener que volver a pedir los dos.
-
+    }
+    static int numerosAmigos(int num1,int num2){
+    return(numerosAmigos(num1,num2));
     }
 }
