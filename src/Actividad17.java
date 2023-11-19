8792903d8ce618e56040557aec5735261bf10ee0
AngelsGV <angels.not99@gmail.com>
2023-11-19 23:56:52 +0100

    Initial commit

--- src/Actividad17.java
+++ src/Actividad17.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; // Se piden datos a usuario.
 public class Actividad17 {
     public static void main(String[] args){
         // Principal. Primero pido números y esclarezco que hacer con la función.
@@ -36,28 +36,35 @@ public class Actividad17 {
 
     public static boolean numerosAmigos(int num1, int num2) {
         //Boolean porque el ejercicio nos pide una respuesta no númerica.
-        int suma1 = 0;
+        int suma1 = 0; //Empezamos los sumatorios por 0.
         int suma2 = 0;
         boolean respuesta = false;
+        // Damos este valor inicial para que luego podamos poner que
+        // si se cumplen las condiciones, la respuesta será verdadera.
 
 
-        for (int i = 1; i < num1; i++) { // Empezamos con i en 1, acaba cuando i llega a num1.
+        for (int i = 1; i < num1; i++) {
+            // Empezamos con i en 1, acaba cuando i llega a num1.
+            // El mismo número no lo vamos a contar como divisor como nos dice en el enunciado.
 
             if (num1 % i == 0) {
                 // Comprueba si i es divisor de num1. Si el resto es igual a 0, es divisor.
-                suma1 += i; // Se suma 1
+                suma1 += i; // Suma los divisores posibles sin contar el mismo número.
             }
 
         }
         for (int i = 1; i < num2; i++) { // Empezamos con i en 1, acaba cuando i llega a num2.
+            // El mismo número no lo vamos a contar como divisor como nos dice en el enunciado.
             if (num2 % i == 0) {
                 // Comprueba si i es divisor de num1. Si el resto es igual a 0, es divisor.
-                suma2 += i; // Se suma 1
+                suma2 += i; // Suma los divisores posibles sin contar el mismo número.
             }
         }
 
         if (suma2 == num1 && suma1 == num2) {
             respuesta = true;
+            //Si la suma de divisores de a es igual al número introducido b,
+            //y la suma de divisores de b es igual al número en a, los números serán amigos.
         }
         System.out.println("La suma de divisores de a es: " + suma1);
         System.out.println("La suma de divisores de b es: " + suma2);
