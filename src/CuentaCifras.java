f14a8b6f3c6193a40b8c42403f45a4a1cb7a5c2a
AngelsGV <angels.not99@gmail.com>
2023-11-27 00:45:30 +0100

    Initial commit

---
+++ src/CuentaCifras.java
@@ -0,0 +1,11 @@
+//Pedir num positivo  y que una función diga cuantas cifras tiene dicho num.
+import java.util.Scanner; // Importamos libreria porque necesitamos introducir un valor.
+public class CuentaCifras {
+    public static void main(String[] args){ //Principal
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Introduce un número entero positivo: ");
+    int num;
+    do{
+        cuentaCifras();
+}
+}}
