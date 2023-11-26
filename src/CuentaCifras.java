43cc6ae517d07b4aba7a00703f5090dd8fec4e59
AngelsGV <angels.not99@gmail.com>
2023-11-27 00:59:50 +0100

    Initial commit

--- src/CuentaCifras.java
+++ src/CuentaCifras.java
@@ -4,8 +4,19 @@ public class CuentaCifras {
     public static void main(String[] args){ //Principal
     Scanner sc = new Scanner(System.in);
     System.out.println("Introduce un número entero positivo: ");
-    int num;
+    int num = 0;
     do{
-        cuentaCifras();
+        cuentaCifras(num);
+    }while (num % 2 == 0); //Cuando sea positivo se producira la lectura de la función que cuanta las cifras del num.
+} static void cuentaCifras(int n){
+        while (n > 0) { //Para abrir el bucle debe ser positivo.
+            int digito = n % 10;
+            // Se utiliza para obtener el dígito más a la derecha de un número entero.
+            System.out.println(digito);
+            n /= 10;
+            //para eliminar el dígito más a la derecha de un número
+            //y preparar el número para el próximo ciclo del bucle.
+        }
+    }
+
 }
-}}
