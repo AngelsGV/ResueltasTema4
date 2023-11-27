5d98bdc776f2ce22c0c228cefa2cc4f8853344fc
AngelsGV <angels.not99@gmail.com>
2023-11-27 22:14:46 +0100

    Initial commit

--- src/Actividad17.java
+++ src/Actividad17.java
@@ -5,24 +5,31 @@ public class Actividad17 {
         Scanner sc = new Scanner(System.in);
         int num1, num2; // Vamos a trabajar con números enteros para sacar los divisores.
 
+        System.out.println("Escribe un número: ");
+        num1 = sc.nextInt();
+        pedirNumero(num1);
 
-        do {
-            System.out.println("Escribe un número: ");
-            num1 = sc.nextInt();
-            if (num1 <= 0 ){ //Si el num1 es menor o igual a 0 da mensaje de error y volvera a pedirlo.
-                System.out.println("Error. Los números tienen que ser positivos.");
-            }
-        }while(num1 <= 0);
+        System.out.println("Escribe otro número: ");
+        num2 = sc.nextInt();
+        pedirNumero(num2);
+
+      //  do {
+         //   System.out.println("Escribe un número: ");
+         //   num1 = sc.nextInt();
+         //   if (num1 <= 0 ){ //Si el num1 es menor o igual a 0 da mensaje de error y volvera a pedirlo.
+        //        System.out.println("Error. Los números tienen que ser positivos.");
+          //  }
+       // }while(num1 <= 0);
         //Bucle 1 para pedir número y que sea positivo. Para no tener que volver a pedir los dos.
 
 
-        do{
-            System.out.println("Escribe otro número: ");
-            num2 = sc.nextInt();
-            if (num2 <= 0){//Si el num2 es menor o igual a 0 da mensaje de error y volvera a pedirlo.
-                System.out.println("Error. Los números tienen que ser positivos.");
-            }
-        }while(  num2 <= 0);
+       // do{
+        //     System.out.println("Escribe otro número: ");
+         //   num2 = sc.nextInt();
+            //if (num2 <= 0){//Si el num2 es menor o igual a 0 da mensaje de error y volvera a pedirlo.
+            //    System.out.println("Error. Los números tienen que ser positivos.");
+          // }
+      //  }while(  num2 <= 0);
         //Bucle 2 para pedir número y que sea positivo. Para no tener que volver a pedir los dos.
 
 
@@ -70,5 +77,14 @@ public class Actividad17 {
         System.out.println("La suma de divisores de b es: " + suma2);
         return (respuesta);
         //Les pongo en la respuesta a pantalla a y b porque he leido después en el enunciado que así las nombraba el ejercicio.
+    }
+    static void pedirNumero (int num){
+        Scanner sc = new Scanner(System.in);
+        do {
+            if (num <= 0 ){ //Si el num1 es menor o igual a 0 da mensaje de error y volvera a pedirlo.
+                System.out.println("Error. Los números tienen que ser positivos.");
+            }
+        }while(num <= 0);
+        //Bucle 1 para pedir número y que sea positivo. Para no tener que volver a pedir los dos.
     }
 }
