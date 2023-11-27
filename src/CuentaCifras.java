2953964b02707e0320924e48566e7f9ccb508c8e
AngelsGV <angels.not99@gmail.com>
2023-11-27 01:29:10 +0100

    Initial commit

--- src/CuentaCifras.java
+++ src/CuentaCifras.java
@@ -3,24 +3,33 @@ import java.util.Scanner; // Importamos libreria porque necesitamos introducir u
 public class CuentaCifras {
     public static void main(String[] args){ //Principal
     Scanner sc = new Scanner(System.in);
+
     System.out.println("Introduce un número entero positivo: ");
     int num =sc.nextInt();
-    do{
-       int cantidad = cuentaCifras(num);
-        System.out.println("El número " + num + " tiene " + (cantidad - 1)+ " cifras.");
-    }while (num % 2 == 0); //Cuando sea positivo se producira la lectura de la función que cuanta las cifras del num.
-}
-static int cuentaCifras(int a){
-        int cifras = 1; // Todos lo núm tienen almenos una cifra.
 
-        if (a == 0) {
-            cifras = 1; // El número 0 tiene una cifra
+    do{
+       int cantidad = cuentaCifras(num) - 1;
+        System.out.println("El número " + num + " tiene " + cantidad + " cifras.");
+    } while (num>0); //Cuando sea positivo se producira la lectura de la función que cuanta las cifras del num.
+}
+
+static int cuentaCifras(int a){
+        int cifras = 1;
+
+        if (a == 0 && a<10) {
+
+            cifras = 1; // El número 0 tiene una cifra, pero no lo contamos como positivo
+
         } else {
+
             while (a != 0) {
                 a = a/ 10;
                 cifras++;
             }
 }return(cifras);
+
+
+        // Cuando el num solo tiene 1 cifra no cierra el bucle. No cierra bucle
 
 
 
