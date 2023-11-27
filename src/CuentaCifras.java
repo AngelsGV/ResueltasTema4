520de545c7f25a98006d27661c0592c325c0e7fd
AngelsGV <angels.not99@gmail.com>
2023-11-27 01:15:27 +0100

    Initial commit

--- src/CuentaCifras.java
+++ src/CuentaCifras.java
@@ -4,19 +4,39 @@ public class CuentaCifras {
     public static void main(String[] args){ //Principal
     Scanner sc = new Scanner(System.in);
     System.out.println("Introduce un número entero positivo: ");
-    int num = 0;
+    int num;
     do{
-        cuentaCifras(num);
+       int cantidad = cuentaCifras(num);
+        System.out.println("El número " + num + " tiene " + cantidad + " cifras.");
     }while (num % 2 == 0); //Cuando sea positivo se producira la lectura de la función que cuanta las cifras del num.
-} static void cuentaCifras(int n){
-        while (n > 0) { //Para abrir el bucle debe ser positivo.
-            int digito = n % 10;
+}
+static int cuentaCifras(int a){
+        int cifras = 1; // Todos lo núm tienen almenos una cifra.
+
+        if (a == 0) {
+            cifras = 1; // El número 0 tiene una cifra
+        } else {
+            while (a != 0) {
+                a = a/ 10;
+                cifras++;
+            }
+}return(cifras);
+
+
+
+
+
+
+//static void cuentaCifras(int n){
+       // while (n > 0) { //Para abrir el bucle debe ser positivo.
+           // int digito = n % 10;
             // Se utiliza para obtener el dígito más a la derecha de un número entero.
-            System.out.println(digito);
-            n /= 10;
+            //System.out.println(digito);
+            //n /= 10;
             //para eliminar el dígito más a la derecha de un número
             //y preparar el número para el próximo ciclo del bucle.
-        }
+       // } Lo he intentado con static int i no sabia como hacerlo
     }
-
 }
+
+
